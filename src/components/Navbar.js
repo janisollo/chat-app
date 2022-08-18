cb9faed8ef0e60eeff6933562afc46c2cc502315
janisollo <sollo.jani@gmail.com>
2022-08-18 11:24:39 +0700

    Firebase auth started

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import { auth } from '../firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { SignInMethod } from 'firebase/auth';
+import SingIn from './SingIn';
 
 const style = {
   nav: `bg-gray-800 h-20 flex justify-between items-center p-4`,
@@ -6,9 +10,11 @@ const style = {
 };
 
 const Navbar = () => {
+  const [user] = useAuthState(auth);
   return (
     <div className={style.nav}>
       <h1 className={style.heading}>Chat app</h1>
+      <SingIn />
     </div>
   );
 };
