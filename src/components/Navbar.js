6701b516839259d7f6202d5c648b4d77ce036607
janisollo <sollo.jani@gmail.com>
2022-08-18 11:43:11 +0700

    Signin and Logout done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { auth } from '../firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { SignInMethod } from 'firebase/auth';
 import SingIn from './SingIn';
+import LogOut from './LogOut';
 
 const style = {
   nav: `bg-gray-800 h-20 flex justify-between items-center p-4`,
@@ -14,7 +14,7 @@ const Navbar = () => {
   return (
     <div className={style.nav}>
       <h1 className={style.heading}>Chat app</h1>
-      <SingIn />
+      {user ? <LogOut /> : <SingIn />}
     </div>
   );
 };
